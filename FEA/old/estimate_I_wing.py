00c1cc3e0b694a6b4512ab719ac2ef8487fba5f3
lorebasket <lore2000.valle@gmail.com>
2026-04-28 16:24:57 +0200

    Update .gitignore

--- FEA/old/estimate_I_wing.py
+++
@@ -1,9 +0,0 @@
-
-def estimate_Iy_Iz_thin_wing(chord, thickness, shape_factor=1.0):
-    
-    b = chord
-    h = thickness
-    Iy = shape_factor * b * h**3 / 12.0
-    Iz = shape_factor * h * b**3 / 12.0
-    
-    return Iy, Iz
